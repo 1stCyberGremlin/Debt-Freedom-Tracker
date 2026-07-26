e2953ab164fc1dc859bbb0e551a5cf0544f38280
1stCyberGremlin <cysyke@gmail.com>
2026-07-26 17:34:03 -0400

    Fix WebView loading with local HTTPS asset origin

--- android-clean/app/src/main/java/com/debtfreedomtracker/app/MainActivity.java
+++ android-clean/app/src/main/java/com/debtfreedomtracker/app/MainActivity.java
@@ -3,9 +3,13 @@ package com.debtfreedomtracker.app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import androidx.webkit.WebViewAssetLoader;
 
 public class MainActivity extends Activity {
     private WebView webView;
@@ -21,17 +25,32 @@ public class MainActivity extends Activity {
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setDatabaseEnabled(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccess(false);
+        settings.setAllowContentAccess(false);
         settings.setMediaPlaybackRequiresUserGesture(false);
         settings.setBuiltInZoomControls(false);
         settings.setDisplayZoomControls(false);
 
+        WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
+                .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
+                .build();
+
         webView.setWebChromeClient(new WebChromeClient());
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                return assetLoader.shouldInterceptRequest(request.getUrl());
+            }
+
+            @Override
+            @SuppressWarnings("deprecation")
+            public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+                return assetLoader.shouldInterceptRequest(android.net.Uri.parse(url));
+            }
+        });
 
         if (savedInstanceState == null) {
-            webView.loadUrl("file:///android_asset/www/index.html");
+            webView.loadUrl("https://appassets.androidplatform.net/assets/www/index.html");
         } else {
             webView.restoreState(savedInstanceState);
         }
